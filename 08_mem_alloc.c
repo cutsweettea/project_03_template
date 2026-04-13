365fc0f214ca715e9cb4b54b5a925e143b30b169
cutsweettea <swagastan@proton.me>
2026-04-13 12:37:02 -0400

    created create array function that allocs memory and fills with elements n+1

--- 08_mem_alloc.c
+++ 08_mem_alloc.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* CreateArray(int length) {
+  int* arr = (int*)malloc(sizeof(int)*length);
+  for(int i = 0; i < length; i++) {
+    arr[i] = i+1;
+  }
+  return arr;
+}
 
 int main(){
   int length;
