47c065e8adff377781050314deb8c00fb1739208
cutsweettea <swagastan@proton.me>
2026-04-13 12:30:27 -0400

    added fib sequence function

--- 05_loops.c
+++ 05_loops.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+const int FIRST_FIB[] = {0, 1};
+
+int Fib(int step) {
+  if(step <= 1) return FIRST_FIB[step];
+  int n1 = FIRST_FIB[0];
+  int n2 = FIRST_FIB[1];
+  int cur = 0;
+  for(int i = 2; i <= step; i++) {
+    cur = n1 + n2;
+    n1 = n2;
+    n2 = cur;
+  }
+  return cur;
+}
+
 int main(){
   int step;
 
