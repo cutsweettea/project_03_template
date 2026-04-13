4db3d04cfb0b129cb9ff769704b41ca0ba742467
cutsweettea <swagastan@proton.me>
2026-04-13 12:07:48 -0400

    added deal damage function, including healing ability

--- 04_conditionals.c
+++ 04_conditionals.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int DealDamage(int damage, int health) {
+  if(damage >= 0) {
+    printf("You dealt %u damage!\n", damage);
+    int diff = health - damage;
+    if(diff > 0) return diff;
+    else {
+      printf("Enemy defeated!\n");
+      return 0;
+    }
+  } else {
+    printf("Enemy healed %u health!\n", (damage*-1));
+    return health - damage;
+  }
+}
+
 int main(){
   int damage;
   int health;
